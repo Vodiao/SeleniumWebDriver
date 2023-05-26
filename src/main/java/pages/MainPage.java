66da86925bf64f5b512543f023949bdd3e8fd54d
Vadym Potorzhynskyi <73354570+Vodiao@users.noreply.github.com>
2023-05-26 20:03:55 +0200

    Get a list of course instructors

---
+++ src/main/java/pages/MainPage.java
@@ -0,0 +1,15 @@
+package pages;
+import org.openqa.selenium.WebDriver;
+
+
+public class MainPage {
+    WebDriver driver;
+
+    public MainPage(WebDriver driver) {
+        this.driver = driver;
+
+
+    }
+
+
+}
